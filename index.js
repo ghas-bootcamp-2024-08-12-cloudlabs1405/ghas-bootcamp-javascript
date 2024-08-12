058530c7a677b3547da7ebef646d755bef8e9fd4
github-cloudlabsuser-1405 <168142842+github-cloudlabsuser-1405@users.noreply.github.com>
2024-08-12 21:08:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var mkdirp = require('mkdirp')
 var fs = require('fs')
 var path = require('path')
 var os = require('os')
+var secret = "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"
 
 var win32 = os.platform() === 'win32'
 
